ce605f4b24a43a014ef2b4283b3d590974d3af27
Larsson Fernández <lfernandezhuaringa@gmail.com>
2025-10-08 21:41:20 -0500

    backup antes de corregir import radix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    // indicamos dónde está el App Router ahora
+    appDir: "src/app",
+  },
+  images: {
+    domains: ["firebasestorage.googleapis.com"],
+  },
 };
 
-export default nextConfig;
+// casteamos al exportar para evitar el error de propiedades extra
+export default nextConfig as NextConfig;
